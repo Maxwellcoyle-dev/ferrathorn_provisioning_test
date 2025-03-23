87ebc85b0486ae2aef1b594ccdf46e95a5fdb64b
Maxwell Henderson <maxwellc.henderson@gmail.com>
2025-03-23 09:52:42 -0400

    added cloudfront OAI + added cleanup and workspace to terraform pipeline

--- modules/cloudfront_distribution/main.tf
+++ modules/cloudfront_distribution/main.tf
@@ -1,19 +1,18 @@
-# modules/cloudfront_distribution/main.tf
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for ${var.origin_id}"
+}
 
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
     domain_name = var.s3_bucket_domain_name
     origin_id   = var.origin_id
 
-    custom_origin_config {
-      http_port                = 80
-      https_port               = 443
-      origin_protocol_policy   = "http-only"
-      origin_ssl_protocols     = ["TLSv1.2"]
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
   }
 
-  enabled = true
+  enabled             = true
   default_root_object = "index.html"
 
   default_cache_behavior {
@@ -21,14 +20,14 @@ resource "aws_cloudfront_distribution" "cdn" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = var.origin_id
 
+    viewer_protocol_policy = "redirect-to-https"
+
     forwarded_values {
       query_string = false
       cookies {
         forward = "none"
       }
     }
-
-    viewer_protocol_policy = "redirect-to-https"
   }
 
   restrictions {
--- modules/s3_website/main.tf
+++ modules/s3_website/main.tf
@@ -1,5 +1,3 @@
-# modules/s3_website/main.tf
-
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
@@ -15,28 +13,25 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-# Bucket Policy (public read via CloudFront)
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow",
-      Principal = "*",
+      Principal = {
+        AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+      },
       Action = ["s3:GetObject"],
-      Resource = ["${aws_s3_bucket.website_bucket.arn}/*"],
-      Condition = {
-        StringEquals = {
-          "AWS:Referer" = var.allowed_referer
-        }
-      }
+      Resource = ["${aws_s3_bucket.website_bucket.arn}/*"]
     }]
   })
+
   depends_on = [aws_s3_bucket_public_access_block.public_access]
 }
